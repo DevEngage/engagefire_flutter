5b7f152bdc43ed20ec1285bc0b8661a566c39e88
James Brinkerhoff <James@devengage.io>
2019-12-21 17:54:22 -0700

    working on provider

--- lib/core/provider.dart
+++ lib/core/provider.dart
@@ -1,4 +1,5 @@
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:engagefire/core/firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -13,16 +14,33 @@ class EngageProvider extends EngageFirestore with ChangeNotifier {
   // final List<String> collections;
   // final List<String> states;
   final String path;
-  Map defaultValue = {};
-  List _list;
+  Map defaultData = {};
 
-  EngageProvider(this.path, [this.defaultValue]):  super(path);
+  EngageProvider(this.path, [this.defaultData]):  super(path);
 
-  // getList() => _list;
-
-  $() {
-
+  $notifyListeners() {
     notifyListeners();
   }
+
+  @override
+  publish(data, String what) {
+    // TODO: implement publish
+    notifyListeners();
+    return super.publish(data, what);
+  }
+  
+  Future<dynamic> saveAndNotifiy(newDoc, {CollectionReference listRef}) async {
+    final doc = await super.save(newDoc, listRef: listRef);
+    notifyListeners();
+    return doc;
+  }
+  
+  Future<dynamic> addAndNotifiy(Map<String, dynamic> newDoc, {dynamic docRef, ignoreInit = false}) async {
+    final doc = await super.add(newDoc, docRef: docRef, ignoreInit: ignoreInit);
+    notifyListeners();
+    return doc;
+  }
+
+
 
 }
